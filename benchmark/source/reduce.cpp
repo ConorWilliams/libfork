e98511d8c5cce025f1fc2d10eb2377ea1f48c43b
Conor Williams <conorwilliams@outlook.com>
2023-02-24 17:07:22 +0000

    reduce with tbb

--- benchmark/source/reduce.cpp
+++ benchmark/source/reduce.cpp
@@ -26,8 +26,8 @@ auto libfork_r(std::span<int> x, std::size_t block) -> lf::basic_task<int, Conte
   auto h = x.size() / 2;
   auto t = x.size() - h;
 
-  auto a = co_await libfork_r<Context>(x.last(t), block).fork();
-  auto b = co_await libfork_r<Context>(x.first(h), block);
+  auto a = co_await libfork_r<Context>(x.first(h), block).fork();
+  auto b = co_await libfork_r<Context>(x.last(h), block);
 
   co_await lf::join();
 
@@ -54,6 +54,29 @@ auto omp_r(std::span<int> x, std::size_t block) -> int {
   return a + b;
 }
 
+int tbb_r(std::span<int> x, std::size_t block) {
+  if (x.size() <= block) {
+    return std::reduce(x.begin(), x.end());
+  }
+
+  auto h = x.size() / 2;
+  auto t = x.size() - h;
+
+  int a, b;
+
+  tbb::task_group g;
+
+  g.run([&] {
+    a = tbb_r(x.first(h), block);
+  });
+
+  b = omp_r(x.last(t), block);
+
+  g.wait();
+
+  return a + b;
+}
+
 }  // namespace
 
 auto benchmark_reduce() -> void {
@@ -71,7 +94,9 @@ auto benchmark_reduce() -> void {
   // bench.maxEpochTime(std::chrono::milliseconds(1000));
   bench.performanceCounters(true);
 
-  std::vector<int> x(1 * 2 * 3 * 4 * 10'000'000);
+  std::size_t chunk = 100'000;
+
+  std::vector<int> x(1 * 2 * 3 * 4 * 10'00'000);
 
   std::iota(x.begin(), x.end(), 0);
 
@@ -87,7 +112,7 @@ auto benchmark_reduce() -> void {
     lf::busy_pool pool{i};
 
     bench.run("busy_pool " + std::to_string(i) + " threads", [&] {
-      auto y = pool.schedule(libfork_r(x, x.size() / (i)));
+      auto y = pool.schedule(libfork_r(x, x.size() / i));
 
       ankerl::nanobench::doNotOptimizeAway(y);
 
@@ -102,13 +127,29 @@ auto benchmark_reduce() -> void {
 #pragma omp parallel num_threads(i)
 #pragma omp single nowait
     bench.run("openMP tasking" + std::to_string(i) + " threads", [&] {
-      ankerl::nanobench::doNotOptimizeAway(omp_r(x, x.size() / (i)));
+      ankerl::nanobench::doNotOptimizeAway(omp_r(x, x.size() / i));
+    });
+  }
+
+  for (int i = 1; i <= std::thread::hardware_concurrency(); ++i) {
+    //
+
+    tbb::task_arena limited(i);
+
+    limited.execute([&] {
+      bench.run("intel TBB " + std::to_string(i) + " threads", [&] {
+        auto y = tbb_r(x, x.size() / i);
+        ankerl::nanobench::doNotOptimizeAway(x);
+        if (y != correct) {
+          throw std::runtime_error("tbb failed");
+        }
+      });
     });
   }
 
   for (std::size_t i = 1; i <= std::thread::hardware_concurrency(); ++i) {
     //
-    bench.run("openMP raw" + std::to_string(i) + " threads", [&] {
+    bench.run("openMP raw " + std::to_string(i) + " threads", [&] {
       int sum = 0;
 #pragma omp parallel for reduction(+ : sum) num_threads(i)
       for (int i = 0; i < x.size(); i++) {
