6db3a28f50f449d41d0c859ec7c92aa5a66de430
Conor Williams <conorwilliams@outlook.com>
2023-02-28 14:03:58 +0000

    fixup __ naming

--- include/libfork/detail/coroutine.hpp
+++ include/libfork/detail/coroutine.hpp
@@ -8,8 +8,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include <__coroutine/coroutine_handle.h>
-#include <__coroutine/trivial_awaitables.h>
 #include <version>
 
 /**
