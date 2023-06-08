6671ee3f3e131f3a3781bf7346d856ce3ec39907
Conor Williams <conorwilliams@outlook.com>
2023-06-08 18:00:07 +1000

    test fail 4

--- test/source/libfork.cpp
+++ test/source/libfork.cpp
@@ -103,7 +103,6 @@ TEST_CASE("libfork", "[libfork]") {
 
   int i = 22;
 
-  //
   auto answer = sync_wait([](auto handle) { handle(); }, fib, i);
 
   std::cout << "fib(" << i << ") = " << answer << "\n";
