ec78ad8c5d65d985ccd02ab3cd6bfdffd96b1ff3
Conor Williams <conorwilliams@outlook.com>
2024-03-12 12:14:45 +0000

    throw macro for -fno-exceptions

--- include/libfork/core/sync_wait.hpp
+++ include/libfork/core/sync_wait.hpp
@@ -250,7 +250,7 @@ LF_CLANG_TLS_NOINLINE auto
 schedule(Sch &&sch, F &&fun, Args &&...args) -> future<async_result_t<F, Args...>> {
   //
   if (impl::tls::has_stack || impl::tls::has_context) {
-    throw schedule_in_worker{};
+    LF_THROW(schedule_in_worker{});
   }
 
   // Initialize the non-worker's stack.
