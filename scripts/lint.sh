f27005d3544717eb06ed454ca7826837af41eed0
conor williams <conorwilliams@outlook.com>
2025-02-15 17:53:06 +0000

    lint fix

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-for file in $(find . \( -name '*.cpp' -o -name '*.hpp' \) -not -path "./.*"); do
-  if [ "${FIX:-false}" = "true" ]; then
-    clang-format -i "$file"
-  else
-    clang-format --dry-run --Werror "$file"
-  fi
-done
+lint_dir() {
+
+  local files=$(find "$1" \( -name '*.cpp' -o -name '*.hpp' \) -not -path "./.*")
+
+  for file in $files; do
+    if [ "${FIX:-false}" = "true" ]; then
+      clang-format -i "$file"
+    else
+      clang-format --dry-run --Werror "$file"
+    fi
+  done
+}
+
+lint_dir "test"
+lint_dir "src"
+lint_dir "include"
