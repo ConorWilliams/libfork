406e2e6bc39e0e15d539d6e956cc317da5c4273f
Conor <conor@myrtle.com>
2026-01-17 11:38:08 +0000

    initial cmake

--- cmake/read_version.cmake
+++ cmake/read_version.cmake
@@ -3,30 +3,14 @@ function(read_version fname)
   file(READ ${fname} ver)
 
   string(REGEX MATCH "#define [A-Z_0-9]*VERSION_MAJOR ([0-9]*)" _ ${ver})
-
   message(STATUS "Found ${fname} majour version: ${CMAKE_MATCH_1}")
-
-  set(version_major
-      ${CMAKE_MATCH_1}
-      PARENT_SCOPE
-  )
+  set(version_major ${CMAKE_MATCH_1} PARENT_SCOPE)
 
   string(REGEX MATCH "#define [A-Z_0-9]*VERSION_MINOR ([0-9]*)" _ ${ver})
-
   message(STATUS "Found ${fname}  minor version: ${CMAKE_MATCH_1}")
-
-  set(version_minor
-      ${CMAKE_MATCH_1}
-      PARENT_SCOPE
-  )
+  set(version_minor ${CMAKE_MATCH_1} PARENT_SCOPE)
 
   string(REGEX MATCH "#define [A-Z_0-9]*VERSION_PATCH ([0-9]*)" _ ${ver})
-
   message(STATUS "Found ${fname}  patch version: ${CMAKE_MATCH_1}")
-
-  set(version_patch
-      ${CMAKE_MATCH_1}
-      PARENT_SCOPE
-  )
-
+  set(version_patch ${CMAKE_MATCH_1} PARENT_SCOPE)
 endfunction()
---
+++ include/libfork/version.hpp
@@ -0,0 +1,20 @@
+/**
+ * @brief __[public]__ The major version of libfork.
+ *
+ * Increments with incompatible API changes.
+ */
+#define LF_VERSION_MAJOR 4
+
+/**
+ * @brief __[public]__ The minor version of libfork.
+ *
+ * Increments when functionality is added in an API backward compatible manner.
+ */
+#define LF_VERSION_MINOR 0
+
+/**
+ * @brief __[public]__ The patch version of libfork.
+ *
+ * Increments when bug fixes are made in an API backward compatible manner.
+ */
+#define LF_VERSION_PATCH 0
