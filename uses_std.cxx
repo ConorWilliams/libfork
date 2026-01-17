061f1f1e9c26b7b1a76c44288b1bed6b6303f36d
Conor <conor@myrtle.com>
2026-01-17 12:21:11 +0000

    formatting

--- uses_std.cxx
+++ uses_std.cxx
@@ -1,3 +1,6 @@
 import std;
 
-void hello_world(std::string const &name) { std::cout << "Hello World! My name is " << name << std::endl; }
+void hello_world(std::string const &name) {
+  // hi
+  std::cout << "Hello World! My name is " << name << std::endl;
+}
