193a7269db96970d60a3ee2c359f8a6be5dc8076
Conor <conor@myrtle.com>
2026-01-21 21:45:40 +0000

    casting checks

--- src/core/promise.cxx
+++ src/core/promise.cxx
@@ -11,29 +11,40 @@ struct promise {
   auto test() -> std::string_view { return "hi"; }
 };
 
-struct basic_frame {
-  basic_frame *parent;
+struct frame_type {
+  frame_type *parent;
 };
 
+static_assert(std::is_standard_layout_v<frame_type>);
+
 template <typename T>
-struct basic_promise;
+struct promise_type;
 
 template <>
-struct basic_promise<void> {
-  basic_frame frame;
+struct promise_type<void> {
+  frame_type frame;
 };
 
+static_assert(alignof(promise_type<void>) == alignof(frame_type));
+
+#ifdef __cpp_lib_is_pointer_interconvertible
+static_assert(std::is_pointer_interconvertible_with_class(&promise_type<void>::frame));
+#else
+static_assert(std::is_standard_layout_v<promise_type<void>>);
+#endif
+
 template <typename T>
-struct basic_promise {
-  basic_frame frame;
+struct promise_type {
+  frame_type frame;
   T *return_address;
 };
 
-static_assert(alignof(basic_promise<int>) == alignof(basic_promise<void>));
+static_assert(alignof(promise_type<int>) == alignof(frame_type));
 
 #ifdef __cpp_lib_is_pointer_interconvertible
-static_assert(std::is_pointer_interconvertible_with_class(&basic_promise<void>::frame));
-static_assert(std::is_pointer_interconvertible_with_class(&basic_promise<long>::frame));
+static_assert(std::is_pointer_interconvertible_with_class(&promise_type<int>::frame));
+#else
+static_assert(std::is_standard_layout_v<promise_type<int>>);
 #endif
 
 } // namespace lf
