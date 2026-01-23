40571cab10f3966cbcca751cb36851f58ae0447c
Conor <conor@myrtle.com>
2026-01-23 21:09:46 +0000

    exception safe

--- benchmark/src/libfork_benchmark/common.hpp
+++ benchmark/src/libfork_benchmark/common.hpp
@@ -3,6 +3,8 @@
 #include <format>
 #include <stdexcept>
 
+#include "libfork/macros.hpp"
+
 /**
  * @class incorrect_result
  * @brief A wrapper around std::runtime_error to indicate an incorrect result.
@@ -15,6 +17,6 @@ struct incorrect_result : public std::runtime_error {
 #define CHECK_RESULT(result, expected)                                                                       \
   do {                                                                                                       \
     if ((result) != (expected)) {                                                                            \
-      throw incorrect_result(std::format("{}={} != {}={}", #expected, (expected), #result, (result)));       \
+      LF_THROW(incorrect_result(std::format("{}={} != {}={}", #expected, (expected), #result, (result))));   \
     }                                                                                                        \
   } while (0)
