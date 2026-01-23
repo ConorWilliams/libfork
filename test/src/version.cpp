7ab7649c79f37df91c7828110a397e31fca3f329
Conor <conor@myrtle.com>
2026-01-23 15:10:17 +0000

    better version test

--- test/src/version.cpp
+++ test/src/version.cpp
@@ -2,12 +2,20 @@
 
 #include "libfork/version.hpp"
 
+#ifndef LF_VERSION_MAJOR
+  #error Expected macro is missing
+#endif
+
+#ifndef LF_VERSION_MINOR
+  #error Expected macro is missing
+#endif
+
+#ifndef LF_VERSION_PATCH
+  #error Expected macro is missing
+#endif
+
 TEST_CASE("Version header", "[version]") {
   constexpr std::size_t major{LF_VERSION_MAJOR};
   constexpr std::size_t minor{LF_VERSION_MINOR};
   constexpr std::size_t patch{LF_VERSION_PATCH};
-
-  REQUIRE(major >= 4)
-  REQUIRE(minor >= 0)
-  REQUIRE(patch >= 0)
 }
