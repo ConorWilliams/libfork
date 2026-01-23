658b6c60a61bf53e00fae816aea4f1d345af3d03
Conor <conor@myrtle.com>
2026-01-23 19:38:47 +0000

    use variables

--- test/src/version.cpp
+++ test/src/version.cpp
@@ -18,4 +18,8 @@ TEST_CASE("Version header", "[version]") {
   constexpr std::size_t major{LF_VERSION_MAJOR};
   constexpr std::size_t minor{LF_VERSION_MINOR};
   constexpr std::size_t patch{LF_VERSION_PATCH};
+
+  REQUIRE(major >= 4);
+  REQUIRE(minor >= 0);
+  REQUIRE(patch >= 0);
 }
