231281ba20e2542b1f465656e0a9510cebdba618
Conor <conor@myrtle.com>
2026-01-24 08:30:19 +0000

    macros

--- include/libfork/macros.hpp
+++ include/libfork/macros.hpp
@@ -1,6 +1,63 @@
 #pragma once
 
 /**
+ * @file macro.hpp
+ *
+ * @brief A collection of internal/public macros.
+ *
+ * These are exhaustively documented due to macros nasty visibility rules
+ * however, only macros that are marked as __[public]__ should be consumed.
+ */
+
+// =============== Inlining/optimisation =============== //
+
+/**
+ * @brief Macro to use next to 'inline' to force a function to be inlined.
+ *
+ * \rst
+ *
+ * .. note::
+ *
+ *    This does not imply the c++'s `inline` keyword which also has an effect on linkage.
+ *
+ * \endrst
+ */
+#if !defined(LF_FORCE_INLINE)
+  #if defined(_MSC_VER) && !defined(__clang__)
+    #define LF_FORCE_INLINE __forceinline
+  #elif defined(__GNUC__) && __GNUC__ > 3
+  // Clang also defines __GNUC__ (as 4)
+    #define LF_FORCE_INLINE __attribute__((__always_inline__))
+  #else
+    #define LF_FORCE_INLINE
+  #endif
+#endif
+
+/**
+ * @brief Macro to prevent a function to be inlined.
+ */
+#if !defined(LF_NO_INLINE)
+  #if defined(_MSC_VER) && !defined(__clang__)
+    #define LF_NO_INLINE __declspec(noinline)
+  #elif defined(__GNUC__) && __GNUC__ > 3
+    // Clang also defines __GNUC__ (as 4)
+    #if defined(__CUDACC__)
+      // nvcc doesn't always parse __noinline__, see: https://svn.boost.org/trac/boost/ticket/9392
+      #define LF_NO_INLINE __attribute__((noinline))
+    #elif defined(__HIP__)
+      // See https://github.com/boostorg/config/issues/392
+      #define LF_NO_INLINE __attribute__((noinline))
+    #else
+      #define LF_NO_INLINE __attribute__((__noinline__))
+    #endif
+  #else
+    #define LF_NO_INLINE
+  #endif
+#endif
+
+// =============== Exception Handling Macros =============== //
+
+/**
  * @brief __[public]__ Detects if the compiler has exceptions enabled.
  *
  * Overridable by defining ``LF_COMPILER_EXCEPTIONS``.
