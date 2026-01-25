8e013a57ecea8791e712605c9724086622532756
Conor <conor@myrtle.com>
2026-01-25 16:10:04 +0000

    initial test

--- benchmark/src/libfork_benchmark/fib/lf_parts.cpp
+++ benchmark/src/libfork_benchmark/fib/lf_parts.cpp
@@ -1,4 +1,6 @@
 #include <coroutine>
+#include <cstddef>
+#include <new>
 
 #include <benchmark/benchmark.h>
 
@@ -12,6 +14,32 @@ struct stack_on_heap {
   static constexpr auto operator new(std::size_t sz) -> void * { return ::operator new(sz); }
   static constexpr auto operator delete(void *p) noexcept -> void { ::operator delete(p); }
 };
+
+struct stack_on_data {
+  static constexpr std::size_t capacity = 1024 * 1024 * 4;
+  thread_local static std::byte buffer[capacity];
+  thread_local static std::size_t offset;
+
+  static auto operator new(std::size_t sz) -> void * {
+    sz = (sz + 15uz) & ~15uz;
+    if (offset + sz > capacity) {
+      throw std::bad_alloc();
+    }
+    void *ptr = &buffer[offset];
+    offset += sz;
+    return ptr;
+  }
+
+  static auto operator delete([[maybe_unused]] void *p) noexcept -> void {}
+
+  static auto operator delete([[maybe_unused]] void *p, std::size_t sz) noexcept -> void {
+    sz = (sz + 15uz) & ~15uz;
+    offset -= sz;
+  }
+};
+
+thread_local std::byte stack_on_data::buffer[stack_on_data::capacity];
+thread_local std::size_t stack_on_data::offset = 0;
 
 template <lf::alloc_mixin StackPolicy>
 constexpr auto no_sym = [](this auto fib, std::int64_t *ret, std::int64_t n) -> lf::task<void, StackPolicy> {
@@ -29,7 +57,8 @@ constexpr auto no_sym = [](this auto fib, std::int64_t *ret, std::int64_t n) -> 
   *ret = lhs + rhs;
 };
 
-constexpr auto sym = [](this auto fib, std::int64_t *ret, std::int64_t n) -> lf::task<void, stack_on_heap> {
+template <lf::alloc_mixin StackPolicy>
+constexpr auto sym = [](this auto fib, std::int64_t *ret, std::int64_t n) -> lf::task<void, StackPolicy> {
   if (n < 2) {
     *ret = n;
     co_return;
@@ -68,5 +97,11 @@ void fib(benchmark::State &state) {
 BENCHMARK(fib<no_sym<stack_on_heap>>)->Name("test/libfork/fib/heap/no_sym_transfer")->Arg(fib_test);
 BENCHMARK(fib<no_sym<stack_on_heap>>)->Name("base/libfork/fib/heap/no_sym_transfer")->Arg(fib_base);
 
-BENCHMARK(fib<sym>)->Name("test/libfork/fib/heap/sym_transfer")->Arg(fib_test);
-BENCHMARK(fib<sym>)->Name("base/libfork/fib/heap/sym_transfer")->Arg(fib_base);
+BENCHMARK(fib<sym<stack_on_heap>>)->Name("test/libfork/fib/heap/sym_transfer")->Arg(fib_test);
+BENCHMARK(fib<sym<stack_on_heap>>)->Name("base/libfork/fib/heap/sym_transfer")->Arg(fib_base);
+
+BENCHMARK(fib<no_sym<stack_on_data>>)->Name("test/libfork/fib/data/no_sym_transfer")->Arg(fib_test);
+BENCHMARK(fib<no_sym<stack_on_data>>)->Name("base/libfork/fib/data/no_sym_transfer")->Arg(fib_base);
+
+BENCHMARK(fib<sym<stack_on_data>>)->Name("test/libfork/fib/data/sym_transfer")->Arg(fib_test);
+BENCHMARK(fib<sym<stack_on_data>>)->Name("base/libfork/fib/data/sym_transfer")->Arg(fib_base);
