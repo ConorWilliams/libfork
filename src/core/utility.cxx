aa1206d4bef7822cc9dac504b8c231402e714f6d
Conor <conor@myrtle.com>
2026-01-25 13:51:54 +0000

    utiliy types

--- src/core/utility.cxx
+++ src/core/utility.cxx
@@ -4,6 +4,28 @@ import std;
 
 namespace lf {
 
-//
+/**
+ * @brief An immovable, empty type.
+ */
+export struct immovable {
+  immovable() = default;
+  immovable(const immovable &) = delete;
+  immovable(immovable &&) = delete;
+  auto operator=(const immovable &) -> immovable & = delete;
+  auto operator=(immovable &&) -> immovable & = delete;
+  ~immovable() = default;
+};
+
+/**
+ * @brief A move-only, empty type.
+ */
+export struct move_only {
+  move_only() = default;
+  move_only(const move_only &) = delete;
+  move_only(move_only &&) = default;
+  auto operator=(const move_only &) -> move_only & = delete;
+  auto operator=(move_only &&) -> move_only & = default;
+  ~move_only() = default;
+};
 
 } // namespace lf
