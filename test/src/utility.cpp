424b4bd80a6a219c18b8c25712010137ea5f3e96
Conor <conor@myrtle.com>
2026-01-25 14:04:43 +0000

    utility tests

--- test/src/utility.cpp
+++ test/src/utility.cpp
@@ -1,36 +1,41 @@
 #include <catch2/catch_test_macros.hpp>
 
 import std;
+
 import libfork.core;
 
-using namespace lf;
+using lf::immovable;
+using lf::move_only;
 
-// immovable properties
-static_assert(std::is_default_constructible_v<immovable>);
-static_assert(!std::is_copy_constructible_v<immovable>);
-static_assert(!std::is_move_constructible_v<immovable>);
-static_assert(!std::is_copy_assignable_v<immovable>);
-static_assert(!std::is_move_assignable_v<immovable>);
-
-// move_only properties
-static_assert(std::is_default_constructible_v<move_only>);
-static_assert(!std::is_copy_constructible_v<move_only>);
-static_assert(std::is_move_constructible_v<move_only>);
-static_assert(!std::is_copy_assignable_v<move_only>);
-static_assert(std::is_move_assignable_v<move_only>);
-
-// Mixin tests
 namespace {
-struct test_immovable : immovable {};
-static_assert(std::is_default_constructible_v<test_immovable>);
-static_assert(!std::is_copy_constructible_v<test_immovable>);
-static_assert(!std::is_move_constructible_v<test_immovable>);
 
+struct test_immovable : immovable {};
 struct test_move_only : move_only {};
-static_assert(std::is_default_constructible_v<test_move_only>);
-static_assert(!std::is_copy_constructible_v<test_move_only>);
-static_assert(std::is_move_constructible_v<test_move_only>);
-static_assert(std::is_move_assignable_v<test_move_only>);
+
 } // namespace
 
-TEST_CASE("Utility properties", "[utility]") { SUCCEED("static_asserts passed"); }
+TEST_CASE("Utility properties", "[utility]") {
+
+  STATIC_REQUIRE(std::is_default_constructible_v<immovable>);
+  STATIC_REQUIRE(!std::is_copy_constructible_v<immovable>);
+  STATIC_REQUIRE(!std::is_move_constructible_v<immovable>);
+  STATIC_REQUIRE(!std::is_copy_assignable_v<immovable>);
+  STATIC_REQUIRE(!std::is_move_assignable_v<immovable>);
+
+  STATIC_REQUIRE(std::is_default_constructible_v<move_only>);
+  STATIC_REQUIRE(!std::is_copy_constructible_v<move_only>);
+  STATIC_REQUIRE(std::is_move_constructible_v<move_only>);
+  STATIC_REQUIRE(!std::is_copy_assignable_v<move_only>);
+  STATIC_REQUIRE(std::is_move_assignable_v<move_only>);
+  STATIC_REQUIRE(std::movable<move_only>);
+
+  STATIC_REQUIRE(std::is_default_constructible_v<test_immovable>);
+  STATIC_REQUIRE(!std::is_copy_constructible_v<test_immovable>);
+  STATIC_REQUIRE(!std::is_move_constructible_v<test_immovable>);
+
+  STATIC_REQUIRE(std::is_default_constructible_v<test_move_only>);
+  STATIC_REQUIRE(!std::is_copy_constructible_v<test_move_only>);
+  STATIC_REQUIRE(std::is_move_constructible_v<test_move_only>);
+  STATIC_REQUIRE(std::is_move_assignable_v<test_move_only>);
+  STATIC_REQUIRE(std::movable<test_move_only>);
+}
