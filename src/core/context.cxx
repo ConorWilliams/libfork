cd243a18d236d898b5c2931d581c902e9105c44c
Conor <conor@myrtle.com>
2026-02-01 18:28:20 +0000

    interfaces

--- src/core/context.cxx
+++ src/core/context.cxx
@@ -7,9 +7,27 @@ import :frame;
 
 namespace lf {
 
+// TODO: private bits / split
 export struct work_handle {
   frame_type *frame;
 };
+
+static_assert(std::atomic<work_handle>::is_always_lock_free);
+
+template <typename T>
+concept context = requires (T &ctx, work_handle h) {
+  { ctx.push(h) };
+  { ctx.pop() } noexcept -> std::same_as<work_handle>;
+};
+
+struct polymorphic_context {
+  virtual void push(work_handle h) = 0;
+  virtual work_handle pop() noexcept = 0;
+
+  virtual ~polymorphic_context() = default;
+};
+
+static_assert(context<polymorphic_context>);
 
 template <typename Context>
 constinit inline thread_local Context *thread_context = nullptr;
