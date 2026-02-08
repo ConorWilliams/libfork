4b35191d21d1d5140f38668998304cbe420f8a82
Conor <conor@myrtle.com>
2026-02-08 14:17:13 +0000

    comment out bits

--- src/core/context.cxx
+++ src/core/context.cxx
@@ -7,28 +7,12 @@ import :frame;
 
 namespace lf {
 
-// TODO: private bits / split
-export struct work_handle {
-  frame_type *frame;
-};
+// export struct polymorphic_context {
+//   virtual void push(work_handle h) = 0;
+//   virtual auto pop() noexcept -> work_handle = 0;
+//   virtual ~polymorphic_context() = default;
+// };
 
-static_assert(std::atomic<work_handle>::is_always_lock_free);
-
-template <typename T>
-concept context = requires (T &ctx, work_handle h) {
-  { ctx.push(h) } -> std::same_as<void>;
-  { ctx.pop() } noexcept -> std::same_as<work_handle>;
-};
-
-export struct polymorphic_context {
-  virtual void push(work_handle h) = 0;
-  virtual auto pop() noexcept -> work_handle = 0;
-  virtual ~polymorphic_context() = default;
-};
-
-static_assert(context<polymorphic_context>);
-
-export template <typename Context>
-constinit inline thread_local Context *thread_context = nullptr;
+// static_assert(context<polymorphic_context>);
 
 } // namespace lf
