8abd638a7094e66371cefa5a8ee6d74013dbb912
Conor <conor@myrtle.com>
2026-03-22 10:24:16 +0000

    spell

--- src/core/schedule.cxx
+++ src/core/schedule.cxx
@@ -8,9 +8,9 @@ import :frame;
 namespace lf {
 
 // schedule(context*, fn, args...) -> heap allocated shared state this must
-// create a task, bind approprate values + cancellation submit the task to a
+// create a task, bind appropriate values + cancellation submit the task to a
 // context. So a context must have a submit(...) method, the thing we submit
-// must be atomicable, ideally it would be able to accpet submission at either
+// must be atomicable, ideally it would be able to accept submission at either
 // a root OR context switch point
 
 struct block_deleter {
