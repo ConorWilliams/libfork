9d2d6926cb9af03ad8baa9eae04305e7687dfd0a
Conor <conor@myrtle.com>
2026-03-27 22:22:42 +0000

    more imports

--- src/core/core.cxx
+++ src/core/core.cxx
@@ -7,7 +7,7 @@ export import :utility;
 export import :tuple;
 
 // T2 partitions
-export import :frame;         // concepts, constants, utility
+export import :frame;         // concepts, constants
 export import :deque;         // concepts, constants, utility
 export import :thread_locals; // concepts
 export import :poly_context;  // concepts
@@ -17,7 +17,7 @@ export import :ops;     // concepts, utility, tuple, frame
 export import :handles; // frame
 
 // T4 partitions
-export import :promise; // concepts, frame, utility, poly_context, thread_locals, ops, handles
+export import :promise; // concepts, frame, utility, thread_locals, ops, handles
 
 // T5 partitions
-export import :schedule; // concepts, frame, poly_context, thread_locals, promise
+export import :schedule; // concepts, frame, thread_locals, promise
