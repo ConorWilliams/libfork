64954a9b7b311830496731b8549c5236cb3e81f3
Conor <conor@myrtle.com>
2026-04-03 22:10:28 +0100

    make handle use a common base

--- src/core/handles.cxx
+++ src/core/handles.cxx
@@ -5,6 +5,7 @@ import std;
 import :frame;
 import :utility;
 import :concepts;
+import :thread_locals;
 
 namespace lf {
 
@@ -21,32 +22,35 @@ namespace lf {
 //  - It has a null value, you can test if it is null
 //  - You can store it in an atomic and it is lock-free
 
+export template <typename T>
+class handle {
+ public:
+  constexpr handle() = default;
+  constexpr auto operator==(handle const &) const noexcept -> bool = default;
+  constexpr explicit operator bool() const noexcept { return m_ptr != nullptr; }
+
+  // Template to prevent circular dependency
+  template <std::same_as<checkpoint_t<allocator_t<T>>> U>
+  constexpr handle(key_t, frame_type<U> *ptr) noexcept : m_ptr{ptr} {}
+
+ private:
+  template <std::same_as<checkpoint_t<allocator_t<T>>> U>
+  [[nodiscard]]
+  constexpr friend auto get(key_t, handle<U> other) noexcept -> frame_type<U> * {
+    return static_cast<frame_type<U> *>(other.m_ptr);
+  }
+
+  frame_base *m_ptr = nullptr;
+};
+
 /**
  * @brief [TODO:description]
  *
  * @tparam T The (potentially incomplete) worker context.
  */
 export template <typename T>
-class frame_handle {
- public:
-  constexpr frame_handle() = default;
-
-  // Template to prevent circular dependency
-  template <std::same_as<checkpoint_t<allocator_t<T>>> U>
-  constexpr frame_handle(key_t, frame_type<U> *ptr) noexcept : m_ptr{ptr} {}
-
-  constexpr auto operator==(frame_handle const &) const noexcept -> bool = default;
-
-  explicit operator bool() const noexcept { return m_ptr != nullptr; }
-
-  template <std::same_as<checkpoint_t<allocator_t<T>>> U>
-  [[nodiscard]]
-  constexpr auto get(key_t) const noexcept -> auto * {
-    return static_cast<frame_type<U> *>(m_ptr);
-  }
-
- private:
-  frame_base *m_ptr = nullptr;
+struct frame_handle : handle<T> {
+  using handle<T>::handle;
 };
 
 // =================== Await =================== //
@@ -56,22 +60,16 @@ class frame_handle {
 // maximum determined by uint16_max
 
 export template <typename T>
-class await_handle {
- public:
-  constexpr await_handle() = default;
-  constexpr await_handle(key_t, frame_base *ptr) noexcept : m_ptr{ptr} {}
+struct await_handle : handle<T> {
 
-  constexpr auto operator==(await_handle const &) const noexcept -> bool = default;
+  using handle<T>::handle;
 
-  explicit operator bool() const noexcept { return m_ptr != nullptr; }
+  constexpr auto resume_on(this await_handle self, T *context) noexcept -> void {
+    LF_ASSUME(context == get_context<T>());
+    get(key(), self);
 
-  [[nodiscard]]
-  constexpr auto get(key_t) const noexcept -> frame_base * {
-    return m_ptr;
+    //
   }
-
- private:
-  frame_base *m_ptr = nullptr;
 };
 
 } // namespace lf
