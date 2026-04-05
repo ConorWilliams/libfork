7137ccdabd9d8113c935341549146545d6d713f9
Conor <conor@myrtle.com>
2026-04-05 19:08:28 +0100

    tidy

--- benchmark/src/libfork_benchmark/fib/libfork.cpp
+++ benchmark/src/libfork_benchmark/fib/libfork.cpp
@@ -11,15 +11,9 @@
 
 namespace {
 
-using lf::env;
-using lf::receiver;
-using lf::scheduler;
-using lf::task;
-using lf::worker_context;
-
 struct fib {
-  template <worker_context Context>
-  static auto operator()(env<Context>, std::int64_t n) -> task<std::int64_t, Context> {
+  template <lf::worker_context Context>
+  static auto operator()(lf::env<Context>, std::int64_t n) -> lf::task<std::int64_t, Context> {
     if (n < 2) {
       co_return n;
     }
@@ -38,7 +32,7 @@ struct fib {
   }
 };
 
-template <scheduler Sch>
+template <lf::scheduler Sch>
 void run(benchmark::State &state) {
 
   std::int64_t n = state.range(0);
@@ -50,7 +44,7 @@ void run(benchmark::State &state) {
 
   for (auto _ : state) {
     benchmark::DoNotOptimize(n);
-    receiver recv = lf::schedule2(scheduler, fib{}, n);
+    lf::receiver recv = lf::schedule2(scheduler, fib{}, n);
     std::int64_t return_value = std::move(recv).get();
     CHECK_RESULT(return_value, expect);
     benchmark::DoNotOptimize(return_value);
@@ -59,25 +53,22 @@ void run(benchmark::State &state) {
 
 } // namespace
 
-using lf::adapt_deque;
-using lf::adapt_vector;
-using lf::deque;
-using lf::derived_poly_context;
-using lf::inline_scheduler;
-using lf::mono_context;
-
-using lf::stacks::geometric;
-
 #define BENCH_ONE(mode, ...)                                                                                 \
   BENCHMARK_TEMPLATE(run, __VA_ARGS__)->Name(#mode "/libfork/fib/" #__VA_ARGS__)->Arg(fib_##mode);
 
 #define BENCH_ALL(...) BENCH_ONE(test, __VA_ARGS__) BENCH_ONE(base, __VA_ARGS__)
 
 template <typename Stack, template <typename> typename Adaptor>
-using real_context = mono_context<Stack, Adaptor>; // TODO: use container
+using real_context = lf::mono_context<Stack, Adaptor>;
 
 template <typename Stack, template <typename> typename Adaptor>
-using poly_context = derived_poly_context<Stack, Adaptor>; // TODO: use container
+using poly_context = lf::derived_poly_context<Stack, Adaptor>;
+
+using lf::adapt_deque;
+using lf::adapt_vector;
+using lf::inline_scheduler;
+
+using lf::stacks::geometric;
 
 BENCH_ALL(inline_scheduler<real_context<geometric<>, adapt_vector>>)
 BENCH_ALL(inline_scheduler<poly_context<geometric<>, adapt_vector>>)
