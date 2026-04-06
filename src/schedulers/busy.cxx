b2ac8a4bb0bb5fb45a5da0aa06f248c7e46439df
Conor <conor@myrtle.com>
2026-04-06 15:46:21 +0100

    sketch

--- src/schedulers/busy.cxx
+++ src/schedulers/busy.cxx
@@ -9,15 +9,18 @@ namespace lf {
 
 export template <bool Polymorphic, worker_stack Stack>
 class busy_scheduler {
+
+  using context = ; // TODO: derived_poly or mono_context
+
  public:
-  using context_type = Context::context_type;
+  using context_type = ; // TODO:
 
   void post(lf::sched_handle<context_type> handle) {
     execute(static_cast<context_type &>(m_context), handle);
   }
 
  private:
-  Context m_context;
+  context m_context;
 };
 
 } // namespace lf
