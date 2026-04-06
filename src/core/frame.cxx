94f6496ac2669aded8f407392a5e5a16c4ad6530
Conor <conor@myrtle.com>
2026-04-06 13:05:10 +0100

    fuse kind and exception bit

--- src/core/frame.cxx
+++ src/core/frame.cxx
@@ -18,9 +18,10 @@ struct cancellation {
 // =================== Frame =================== //
 
 // TODO: remove this and other exports
-export enum class category : std::uint8_t {
+
+export enum category : std::uint8_t {
   call = 0,
-  fork,
+  fork = 1,
 };
 
 export struct frame_base {};
@@ -43,10 +44,12 @@ struct frame_type : frame_base {
   [[no_unique_address]]
   Checkpoint stack_ckpt;
 
-  ATOMIC_ALIGN(std::uint32_t) joins = 0;        // Atomic is 32 bits for speed
-  std::uint16_t steals = 0;                     // In debug do overflow checking
-  category kind = static_cast<category>(0);     // Fork/Call
-  ATOMIC_ALIGN(std::uint8_t) exception_bit = 0; // Atomically set
+  // TODO: in theory the 2 bits of information in kind_and_except
+  // could be compressed into one of the pointers?
+
+  ATOMIC_ALIGN(std::uint32_t) joins = 0;          // Atomic is 32 bits for speed
+  std::uint16_t steals = 0;                       // In debug do overflow checking
+  ATOMIC_ALIGN(std::uint8_t) kind_and_except = 0; // Stores fork/call in low bit, exception flag in next bit
 
   // == Member functions == //
 
@@ -69,6 +72,21 @@ struct frame_type : frame_base {
   }
 
   [[nodiscard]]
+  constexpr auto has_exception() const noexcept -> bool {
+    return kind_and_except >= 2;
+  }
+
+  /**
+   * @brief Returns true if caller was first to set exception bit.
+   */
+  [[nodiscard]]
+  constexpr auto atomic_set_exception() noexcept -> bool {
+    // No synchronization is done via exception_bit, hence we can use relaxed atomics
+    // and rely on the usual fork/join synchronization to ensure memory ordering.
+    return atomic_except().fetch_or(0b00000010, std::memory_order_relaxed) < 2;
+  }
+
+  [[nodiscard]]
   constexpr auto handle() LF_HOF(std::coroutine_handle<frame_type>::from_promise(*this))
 
   [[nodiscard]]
@@ -76,14 +94,15 @@ struct frame_type : frame_base {
     return std::atomic_ref{joins};
   }
 
-  [[nodiscard]]
-  constexpr auto atomic_except() noexcept -> std::atomic_ref<std::uint8_t> {
-    return std::atomic_ref{exception_bit};
-  }
-
   constexpr void reset_counters() noexcept {
     joins = k_u16_max;
     steals = 0;
+  }
+
+ private:
+  [[nodiscard]]
+  constexpr auto atomic_except() noexcept -> std::atomic_ref<std::uint8_t> {
+    return std::atomic_ref{kind_and_except};
   }
 };
 
