715ff29f7898c1250814452e3c2febeffc1c5547
Conor <conor@myrtle.com>
2026-04-06 12:39:32 +0100

    uninitialized

---
+++ src/utils/uninitialized.cxx
@@ -0,0 +1,38 @@
+
+export module libfork.utils:uninitialized;
+
+import std;
+
+namespace lf {
+
+// TODO: use trivial union
+
+export template <typename T>
+class uninitialized {
+ public:
+  constexpr uninitialized() = default;
+
+  constexpr uninitialized(uninitialized const &) = delete;
+  constexpr uninitialized(uninitialized &&) = delete;
+
+  constexpr auto operator=(uninitialized const &) -> uninitialized & = delete;
+  constexpr auto operator=(uninitialized &&) -> uninitialized & = delete;
+
+  constexpr ~uninitialized() = default;
+
+  auto operator->() noexcept -> T * { return std::launder(reinterpret_cast<T *>(buffer)); }
+
+  auto operator*() noexcept -> T & { return **this; }
+
+  template <class... Args>
+  auto construct(Args &&...args) noexcept(std::is_nothrow_constructible_v<T, Args...>) -> T & {
+    return *::new (static_cast<void *>(buffer)) T(std::forward<Args>(args)...);
+  }
+
+  void destroy() noexcept { this->~T(); }
+
+ private:
+  alignas(std::max_align_t) std::byte buffer[sizeof(std::max_align_t)];
+};
+
+} // namespace lf
