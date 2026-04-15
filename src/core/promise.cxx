767eb38e864a4e44f67e6646f49c712d9104e451
Conor <conor@myrtle.com>
2026-04-15 16:25:28 +0100

    Revert "full fused release"
    This reverts commit 764b6d87f3978269cdbb9746b4c4dc7c9b19b191.

--- src/core/promise.cxx
+++ src/core/promise.cxx
@@ -112,12 +112,7 @@ constexpr auto final_suspend_full(Context &context, frame_t<Context> *frame) noe
 
     // As soon as we do the fetch_sub (if we loose) someone may acquire
     // the stack so we must prepare it for release now.
-    if (owner) {
-      auto release_key = context.stack().prepare_release();
-      // We were unable to resume the parent and we were its owner, as the
-      // resuming thread will take ownership of the parent's we must give it up.
-      context.stack().release(std::move(release_key));
-    }
+    auto release_key = context.stack().prepare_release();
 
     // TODO: we could add an `if (owner)` around acquire below, then we could
     // define that acquire is always called with null or not-self.
@@ -129,8 +124,10 @@ constexpr auto final_suspend_full(Context &context, frame_t<Context> *frame) noe
       // parent. As we won the race, acquire all writes before resuming.
       std::atomic_thread_fence(std::memory_order_acquire);
 
-      // In case of scenario (2) we must acquire the parent's stack.
-      context.stack().acquire(std::as_const(parent->stack_ckpt));
+      if (owner) {
+        // In case of scenario (2) we must acquire the parent's stack.
+        context.stack().acquire(std::as_const(parent->stack_ckpt));
+      }
 
       // Must reset parent's control block before resuming parent.
       parent->reset_counters();
@@ -144,6 +141,12 @@ constexpr auto final_suspend_full(Context &context, frame_t<Context> *frame) noe
         }
         frame = parent;
         continue;
+      }
+
+      if (owner) {
+        // We were unable to resume the parent and we were its owner, as the
+        // resuming thread will take ownership of the parent's we must give it up.
+        context.stack().release(std::move(release_key));
       }
 
       return parent->handle();
@@ -200,17 +203,15 @@ constexpr auto final_suspend2(Context &context, frame_t<Context> *parent) noexce
 
   bool const owner = parent->stack_ckpt == context.stack().checkpoint();
 
-  if (owner) {
-
-    auto release_key = context.stack().prepare_release();
-    context.stack().release(std::move(release_key));
-  }
+  auto release_key = context.stack().prepare_release();
 
   if (parent->atomic_joins().fetch_sub(1, std::memory_order_release) == 1) {
 
     std::atomic_thread_fence(std::memory_order_acquire);
 
-    context.stack().acquire(std::as_const(parent->stack_ckpt));
+    if (!owner) {
+      context.stack().acquire(std::as_const(parent->stack_ckpt));
+    }
 
     parent->reset_counters();
 
@@ -223,6 +224,10 @@ constexpr auto final_suspend2(Context &context, frame_t<Context> *parent) noexce
       return final_suspend_full<Context>(context, parent);
     }
     return parent->handle();
+  }
+
+  if (owner) {
+    context.stack().release(std::move(release_key));
   }
 
   return std::noop_coroutine();
