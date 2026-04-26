3262a094ef7f7851263f71d54f2a26fe5d267cc4
Conor <conor@myrtle.com>
2026-04-26 10:52:21 +0100

    initiall pass on the awaitable

--- src/core/concepts/awaitable.cxx
+++ src/core/concepts/awaitable.cxx
@@ -1,16 +1,45 @@
 
+module;
+#include "libfork/__impl/utils.hpp"
 export module libfork.core:concepts_awaitable;
+
+import std;
 
 import :handles;
 import :concepts_context;
 
 namespace lf {
 
-template <typename T, typename Context>
-concept awaitable = worker_context<Context> && requires (T &&val, Context &ctx, sched_handle<Context> h) {
-  { static_cast<T &&>(val).await_ready() } -> std::convertible_to<bool>;
-  { static_cast<T &&>(val).await_suspend(h, ctx) } -> std::same_as<void>;
-  { static_cast<T &&>(val).await_resume() };
+/**
+ * @brief Specifies that a cv-ref stripped type is constructible from `T`.
+ */
+template <typename T>
+concept storable = std::constructible_from<std::remove_cvref_t<T>, T &&>;
+
+/**
+ * @brief  Specifies the requirements for a context-switching awaitable type.
+ */
+export template <typename T, typename U>
+concept awaitable =
+    storable<T> && worker_context<U> && requires (std::remove_cvref_t<T> x, U &ctx, sched_handle<U> handle) {
+      { x.await_ready() } -> std::convertible_to<bool>;
+      { x.await_suspend(handle, ctx) } -> std::same_as<void>;
+      { x.await_resume() };
+    };
+
+template <worker_context Context, awaitable<Context> T>
+struct context_switch_awaitable {
+
+  static_assert(plain_object<T>, "Expecting remove cv-ref");
+
+  [[no_unique_address]]
+  T value;
+
+  constexpr auto await_ready() LF_HOF(value.await_ready())
+
+  constexpr void await_suspend(std::coroutine_handle<>);
+
+  constexpr auto await_resume() LF_HOF(std::forward<T>(value).await_resume())
 };
 
 } // namespace lf
