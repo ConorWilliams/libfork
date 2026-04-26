76b800133bd3886bc40ac75b1fa0ae4b27d8e16c
Conor <conor@myrtle.com>
2026-04-26 21:39:34 +0100

    refactor for sub-function

--- src/core/execute.cxx
+++ src/core/execute.cxx
@@ -51,6 +51,34 @@ export struct steal_overflow_error final : libfork_exception {
   }
 };
 
+/**
+ * @brief Consume a steal handle, marks it as stolen and retuns the handle of the stolen task.
+ *
+ * The current thread must resume the handle.
+ *
+ * May throw `steal_overflow_error` if the task has been stolen enough tines to
+ * overflow the steal counter.
+ */
+template <worker_context Context>
+constexpr auto consume(steal_handle<Context> handle) -> std::coroutine_handle<> {
+
+  auto *frame = static_cast<frame_type<checkpoint_t<Context>> *>(get(key(), handle));
+
+  if (frame->steals == k_u16_max) {
+    LF_THROW(steal_overflow_error{});
+  }
+
+  frame->steals += 1;
+
+  return frame->handle();
+}
+
+/**
+ * @brief Bind this thread to a context and execute the scheduled tasks on that context/thread.
+ *
+ * This should not be called from a thread already bound to a context, once this call returns
+ * the thread is unbound from the context.
+ */
 export template <worker_context Context>
 constexpr void execute(Context &context, steal_handle<Context> handle) {
 
@@ -64,15 +92,7 @@ constexpr void execute(Context &context, steal_handle<Context> handle) {
     thread_local_context<Context> = nullptr;
   };
 
-  auto *frame = static_cast<frame_type<checkpoint_t<Context>> *>(get(key(), handle));
-
-  // TODO: bench if we should do this in debug only
-  if (frame->steals == k_u16_max) {
-    LF_THROW(steal_overflow_error{});
-  }
-
-  frame->steals += 1;
-  frame->handle().resume();
+  consume(handle).resume();
 }
 
 } // namespace lf
