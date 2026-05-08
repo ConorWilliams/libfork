90b94e375f4d4b847ab2bbadf1913d07f41b3e7a
Conor <conor@myrtle.com>
2026-05-08 18:32:15 +0100

    TMP benchmarks

--- benchmark/src/serial/fib.cpp
+++ benchmark/src/serial/fib.cpp
@@ -72,7 +72,54 @@ void fib_serial_return(benchmark::State &state) {
   }
 }
 
+constexpr std::size_t m = 10'000;
+
+template <typename = void>
+void accumulate(benchmark::State &state) {
+
+  std::int64_t n = state.range(0);
+  std::int64_t expect = fib_ref(n);
+
+  state.counters["n"] = static_cast<double>(n);
+
+  std::vector<float> data(m);
+
+  for (std::size_t i = 0; i < m; ++i) {
+    data[i] = 2 * i + i % 3;
+  }
+
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(n);
+    auto result = std::accumulate(data.begin(), data.end(), std::size_t(0));
+    benchmark::DoNotOptimize(result);
+  }
+}
+
+template <typename = void>
+void reduce(benchmark::State &state) {
+
+  std::int64_t n = state.range(0);
+  std::int64_t expect = fib_ref(n);
+
+  state.counters["n"] = static_cast<double>(n);
+
+  std::vector<float> data(m);
+
+  for (std::size_t i = 0; i < m; ++i) {
+    data[i] = 2 * i + i % 3;
+  }
+
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(n);
+    auto result = std::reduce(data.begin(), data.end(), std::size_t(0));
+    benchmark::DoNotOptimize(result);
+  }
+}
+
 } // namespace
+
+BENCH_ALL(reduce, serial, add_red, fib)
+BENCH_ALL(accumulate, serial, add_acc, fib)
 
 BENCH_ALL(fib_serial, serial, fib, fib)
 BENCH_ALL(fib_serial_return, serial, fib / return, fib)
