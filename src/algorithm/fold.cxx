2371e0b08a914ed3c40f61bc85ef0ea22e408a9b
Conor <conor@myrtle.com>
2026-05-09 17:10:23 +0100

    new style
    tmp2

--- src/algorithm/fold.cxx
+++ src/algorithm/fold.cxx
@@ -17,78 +17,105 @@ concept indirectly_foldable =
 struct fold_impl {
  private:
   template <typename T>
-  using iter_difference_t = std::iter_difference_t<T>;
+  using iter_diff_t = std::iter_difference_t<T>;
 
   template <typename T>
-  using range_difference_t = std::ranges::range_difference_t<T>;
+  using range_diff_t = std::ranges::range_difference_t<T>;
+
+  template <typename Context, typename I, typename Proj, typename Bop>
+  using result_t = lf::indirect_semigroup_t<Bop, Context, projected<Context, I, Proj>>;
+
+  template <typename Context, typename I, typename Proj, typename Bop>
+  using task_t = lf::task<result_t<Context, I, Proj, Bop>, Context>;
 
  public:
   // (1) iterator-pair, chunk size n >= 1
-  template <worker_context Context,
-            std::random_access_iterator I,
-            std::sized_sentinel_for<I> S,
-            typename T,
-            typename Bop,
-            projectable<Context, I> Proj = std::identity>
-    requires indirectly_foldable<Bop, Context, projected<Context, I, Proj>>
-  static auto operator()(
-      env<Context> /* env */, I head, S tail, iter_difference_t<I> n, T identity, Bop bop, Proj proj = {})
-      -> lf::task<T, Context> {
+  template <worker_context X,                                 //
+            std::random_access_iterator I,                    //
+            std::sized_sentinel_for<I> S,                     //
+            projectable<X, I> Proj = std::identity,           //
+            indirectly_foldable<X, projected<X, I, Proj>> Bop //
+            >
+  static auto
+  operator()(env<X>, I head, S tail, iter_diff_t<I> n, Bop bop, Proj proj = {}) -> task_t<X, I, Proj, Bop> {
 
     LF_ASSUME(n > 0);
 
     auto len = tail - head;
 
-    LF_ASSUME(len >= 0);
+    LF_ASSUME(len > 0);
 
     if (len <= n) {
-      // Leaf: sequential fold over chunk, seeded with `identity`.
-      auto sc = co_await scope();
-      T acc = std::move(identity);
 
-      for (; head != tail; ++head) {
-        if constexpr (async::indirectly_regular_unary_invocable<Proj, Context, I>) {
-          using PV = typename projected<Context, I, Proj>::value_type;
-          PV pv;
-          co_await sc.call(&pv, proj, *head);
+      if constexpr (async::indirectly_regular_unary_invocable<Proj, X, I>) {
+
+        // TODO: could optimize if this type == accumulator type
+        async_result_t<Proj &, X, std::iter_reference_t<I>> init;
+        {
+          auto sc = co_await scope();
+          co_await sc.call(&init, proj, *head);
           co_await sc.join();
-          if constexpr (async_invocable<Bop &, Context, T, PV>) {
-            co_await sc.call(&acc, bop, std::move(acc), std::move(pv));
+        }
+
+        result_t<X, I, Proj, Bop> acc = std::move(init);
+
+        for (++head; head != tail; ++head) {
+
+          async_result_t<Proj &, X, std::iter_reference_t<I>> tmp;
+          {
+            auto sc = co_await scope();
+            co_await sc.call(&init, proj, *head);
+            co_await sc.join();
+          }
+
+          if constexpr (async::indirect_semigroup<Bop, X, projected<X, I, Proj>>) {
+            auto sc = co_await scope();
+            co_await sc.call(&acc, bop, std::move(acc), std::move(tmp));
             co_await sc.join();
           } else {
-            acc = std::invoke(bop, std::move(acc), std::move(pv));
+            acc = std::invoke(bop, std::move(acc), std::move(tmp));
           }
-        } else {
-          using PV = std::remove_cvref_t<std::invoke_result_t<Proj &, std::iter_reference_t<I>>>;
-          if constexpr (async_invocable<Bop &, Context, T, PV>) {
+        }
+
+      } else {
+
+        result_t<X, I, Proj, Bop> acc = proj(*head);
+
+        for (++head; head != tail; ++head) {
+          if constexpr (async::indirect_semigroup<Bop, X, projected<X, I, Proj>>) {
+            auto sc = co_await scope();
             co_await sc.call(&acc, bop, std::move(acc), std::invoke(proj, *head));
             co_await sc.join();
           } else {
             acc = std::invoke(bop, std::move(acc), std::invoke(proj, *head));
           }
         }
+
+        co_return acc;
       }
-      co_return acc;
     }
 
-    // Split. Storage is copy-init'd from `identity` (avoids a default_initializable<T>
-    // requirement); `*return_addr = result` in the child's promise overwrites it.
     auto mid = head + (len / 2);
-    auto sc = co_await scope();
-    T lhs(identity);
-    T rhs(identity);
-    co_await sc.fork(&lhs, fold_impl{}, head, mid, n, identity, bop, proj);
-    co_await sc.call(&rhs, fold_impl{}, mid, tail, n, identity, std::move(bop), std::move(proj));
-    co_await sc.join();
 
-    if constexpr (async_invocable<Bop &, Context, T, T>) {
+    result_t<X, I, Proj, Bop> lhs;
+    result_t<X, I, Proj, Bop> rhs;
+
+    {
+      auto sc = co_await scope();
+      co_await sc.fork(&lhs, fold_impl{}, head, mid, n, bop, proj);
+      co_await sc.call(&rhs, fold_impl{}, mid, tail, n, bop, std::move(proj));
+      co_await sc.join();
+    }
+
+    if constexpr (async::indirect_semigroup<Bop, X, projected<X, I, Proj>>) {
       // TODO: can this be a tail call / use current return address?
+      auto sc = co_await scope();
       co_await sc.call(&lhs, bop, std::move(lhs), std::move(rhs));
       co_await sc.join();
+      co_return lhs;
     } else {
-      lhs = std::invoke(bop, std::move(lhs), std::move(rhs));
+      co_return std::invoke(bop, std::move(lhs), std::move(rhs));
     }
-    co_return lhs;
   }
 
   // (2) iterator-pair, n == 1 specialization (no n parameter)
@@ -160,8 +187,8 @@ struct fold_impl {
             typename Bop,
             typename Proj = std::identity>
     requires indirectly_foldable<Bop, Context, projected<Context, std::ranges::iterator_t<Range>, Proj>>
-  static auto operator()(
-      env<Context> context, Range &&range, range_difference_t<Range> n, T identity, Bop bop, Proj proj = {})
+  static auto
+  operator()(env<Context> context, Range &&range, range_diff_t<Range> n, T identity, Bop bop, Proj proj = {})
       -> lf::task<T, Context> {
     if (n == 1) {
       return fold_impl{}(context,
