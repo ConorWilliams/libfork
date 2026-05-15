c682dbfaff26cbb8f8eab7d4874c0e6da377fac0
Conor <conor@myrtle.com>
2026-05-15 23:30:46 +0100

    tidy up

--- benchmark/src/libfork/skynet.cpp
+++ benchmark/src/libfork/skynet.cpp
@@ -10,9 +10,12 @@ import libfork;
 
 namespace {
 
+constexpr auto n = skynet_branching - 1;
+
 struct skynet_fn {
   template <lf::worker_context Context>
   static auto operator()(lf::env<Context>, std::int64_t num, int depth) -> lf::task<std::int64_t, Context> {
+
     if (depth == 0) {
       co_return num;
     }
@@ -22,16 +25,10 @@ struct skynet_fn {
 
     auto sc = co_await lf::scope();
 
-    for (int i = 0; i < skynet_branching; ++i) {
-      auto const child_num = num + i * sub;
-      auto *result = &children[static_cast<std::size_t>(i)];
-
-      if (i + 1 == skynet_branching) {
-        co_await sc.call(result, skynet_fn{}, child_num, depth - 1);
-      } else {
-        co_await sc.fork(result, skynet_fn{}, child_num, depth - 1);
-      }
+    for (int i = 0; i < n; ++i) {
+      co_await sc.fork(&children[i], skynet_fn{}, num + i * sub, depth - 1);
     }
+    co_await sc.call(&children[n], skynet_fn{}, num + n * sub, depth - 1);
 
     co_await sc.join();
 
