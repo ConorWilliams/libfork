0cf3d4d078c80b70cd7c13fc12e8b1b0306b071f
Conor <conor@myrtle.com>
2026-05-18 21:42:29 +0100

    matmul v2

--- benchmark/src/libfork/matmul.cpp
+++ benchmark/src/libfork/matmul.cpp
@@ -10,43 +10,110 @@ import libfork;
 
 namespace {
 
-template <bool Add>
-struct matmul_fn {
+enum matmul_quadrant : unsigned {
+  matmul_q00,
+  matmul_q10,
+  matmul_q01,
+  matmul_q11,
+};
+
+struct matmul_product_task;
+
+template <matmul_quadrant Quadrant>
+struct matmul_quadrant_task;
+
+struct matmul_product_task {
   template <lf::worker_context Context>
   static auto operator()(lf::env<Context>, float const *A, float const *B, float *R, unsigned n, unsigned s)
-      -> lf::task<void, Context> {
+      -> lf::task<void, Context>;
+};
 
-    if (n <= matmul_cutoff) {
-      matmul_basecase_multiply<Add>(A, B, R, n, s);
-      co_return;
-    }
+template <matmul_quadrant Quadrant>
+struct matmul_quadrant_task {
+  template <lf::worker_context Context>
+  static auto operator()(lf::env<Context>, float const *A, float const *B, float *R, unsigned n, unsigned s)
+      -> lf::task<void, Context>;
+};
 
-    unsigned m = n / 2;
+template <lf::worker_context Context>
+auto matmul_product_task::operator()(
+    lf::env<Context>, float const *A, float const *B, float *R, unsigned n, unsigned s)
+    -> lf::task<void, Context> {
+  if (n <= matmul_cutoff) {
+    matmul_basecase_multiply<true>(A, B, R, n, s);
+    co_return;
+  }
 
-    unsigned o00 = 0;
-    unsigned o01 = m;
-    unsigned o10 = m * s;
-    unsigned o11 = m * s + m;
+  auto sc = co_await lf::scope();
+  co_await sc.fork(matmul_quadrant_task<matmul_q00>{}, A, B, R, n, s);
+  co_await sc.fork(matmul_quadrant_task<matmul_q10>{}, A, B, R, n, s);
+  co_await sc.fork(matmul_quadrant_task<matmul_q01>{}, A, B, R, n, s);
+  co_await sc.call(matmul_quadrant_task<matmul_q11>{}, A, B, R, n, s);
+  co_await sc.join();
+}
 
+template <matmul_quadrant Quadrant>
+template <lf::worker_context Context>
+auto matmul_quadrant_task<Quadrant>::operator()(
+    lf::env<Context>, float const *A, float const *B, float *R, unsigned n, unsigned s)
+    -> lf::task<void, Context> {
+  unsigned m = n / 2;
+
+  unsigned o00 = 0;
+  unsigned o01 = m;
+  unsigned o10 = m * s;
+  unsigned o11 = m * s + m;
+
+  if constexpr (Quadrant == matmul_q00) {
     {
       auto sc = co_await lf::scope();
-      co_await sc.fork(matmul_fn<Add>{}, A + o00, B + o00, R + o00, m, s);
-      co_await sc.fork(matmul_fn<Add>{}, A + o10, B + o00, R + o10, m, s);
-      co_await sc.fork(matmul_fn<Add>{}, A + o00, B + o01, R + o01, m, s);
-      co_await sc.call(matmul_fn<Add>{}, A + o10, B + o01, R + o11, m, s);
+      co_await sc.call(matmul_product_task{}, A + o00, B + o00, R + o00, m, s);
       co_await sc.join();
     }
 
     {
       auto sc = co_await lf::scope();
-      co_await sc.fork(matmul_fn<true>{}, A + o01, B + o10, R + o00, m, s);
-      co_await sc.fork(matmul_fn<true>{}, A + o01, B + o11, R + o01, m, s);
-      co_await sc.fork(matmul_fn<true>{}, A + o11, B + o10, R + o10, m, s);
-      co_await sc.call(matmul_fn<true>{}, A + o11, B + o11, R + o11, m, s);
+      co_await sc.call(matmul_product_task{}, A + o01, B + o10, R + o00, m, s);
+      co_await sc.join();
+    }
+  } else if constexpr (Quadrant == matmul_q10) {
+    {
+      auto sc = co_await lf::scope();
+      co_await sc.call(matmul_product_task{}, A + o10, B + o00, R + o10, m, s);
+      co_await sc.join();
+    }
+
+    {
+      auto sc = co_await lf::scope();
+      co_await sc.call(matmul_product_task{}, A + o11, B + o10, R + o10, m, s);
+      co_await sc.join();
+    }
+  } else if constexpr (Quadrant == matmul_q01) {
+    {
+      auto sc = co_await lf::scope();
+      co_await sc.call(matmul_product_task{}, A + o00, B + o01, R + o01, m, s);
+      co_await sc.join();
+    }
+
+    {
+      auto sc = co_await lf::scope();
+      co_await sc.call(matmul_product_task{}, A + o01, B + o11, R + o01, m, s);
+      co_await sc.join();
+    }
+  } else {
+    {
+      auto sc = co_await lf::scope();
+      co_await sc.call(matmul_product_task{}, A + o10, B + o01, R + o11, m, s);
+      co_await sc.join();
+    }
+
+    {
+      auto sc = co_await lf::scope();
+      co_await sc.call(matmul_product_task{}, A + o11, B + o11, R + o11, m, s);
       co_await sc.join();
     }
   }
-};
+}
 
 template <lf::scheduler Sch>
 void run(benchmark::State &state) {
@@ -54,7 +121,7 @@ void run(benchmark::State &state) {
   Sch scheduler = make_scheduler<Sch>(state);
 
   run_matmul(state, threads, 1e-5f, [&](float const *A, float const *B, float *C, unsigned n) {
-    lf::schedule(scheduler, matmul_fn<false>{}, A, B, C, n, n).get();
+    lf::schedule(scheduler, matmul_product_task{}, A, B, C, n, n).get();
   });
 }
 
